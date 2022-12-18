2b2db603da40033b793eed3e4e9b09ee43bb2187
Aadhithya <aadhithyasan024@gmail.com>
2022-12-19 05:18:21 +0530

    Update

--- app/src/main/java/com/pettikadai/pettikadai_vendor/CartFragment.java
+++ app/src/main/java/com/pettikadai/pettikadai_vendor/CartFragment.java
@@ -34,137 +34,137 @@ public class CartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
-        rcvData = view.findViewById(R.id.rcvData);
-        db = FirebaseFirestore.getInstance();
-        if (shopModels.isEmpty()){
-            Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
-        }
-        db.collection("Shop").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if(error==null){
-                    List<ShopModel> data = value.toObjects(ShopModel.class);
-                    shopModels.addAll(data);
-                    ShopModel dataModel = shopModels.get(0);
-                    Toast.makeText(getContext(), dataModel.getShopName(), Toast.LENGTH_SHORT).show();
-                }
-//                Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
-            }
-        });
-        allDataModels = new ArrayList<>();
-        allDataModels.clear();
-        for (ShopModel shopModel:shopModels){
-            String shopName1 = shopModel.getShopName();
-            productModels.clear();
-            db.collection("Shop/"+shopName1+"/Products").addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                    if(error==null){
-                        List<ProductModel> data = value.toObjects(ProductModel.class);
-                        productModels.addAll(data);
-                        for (ProductModel productModel:productModels){
-                            AllDataModel temp = new AllDataModel(shopModel.getShopName(), shopModel.getShopLocation(), shopModel.getContactNumber(), productModel.getProductName(), productModel.getPrice(), productModel.getQuantity());
-                            allDataModels.add(temp);
-                        }
-                        if (productModels.isEmpty()) {
-                            Toast.makeText(getContext(), "No data Found", Toast.LENGTH_SHORT).show();
-                        }
-                        ProductModel productModel = productModels.get(0);
-                        Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-        rcvData.setLayoutManager(new LinearLayoutManager(getContext()));
-        rcvData.setAdapter(new AllDataAdaptor(getContext(), allDataModels));
-//        if (allDataModels.isEmpty()){
-//            Toast.makeText(getContext(), "No data found", Toast.LENGTH_SHORT).show();
-//        }
-        searchView = view.findViewById(R.id.searchView);
-        searchView.clearFocus();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
+//         rcvData = view.findViewById(R.id.rcvData);
+//         db = FirebaseFirestore.getInstance();
+//         if (shopModels.isEmpty()){
+//             Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
+//         }
+//         db.collection("Shop").addSnapshotListener(new EventListener<QuerySnapshot>() {
+//             @Override
+//             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                 if(error==null){
+//                     List<ShopModel> data = value.toObjects(ShopModel.class);
+//                     shopModels.addAll(data);
+//                     ShopModel dataModel = shopModels.get(0);
+//                     Toast.makeText(getContext(), dataModel.getShopName(), Toast.LENGTH_SHORT).show();
+//                 }
+// //                Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
+//             }
+//         });
+//         allDataModels = new ArrayList<>();
+//         allDataModels.clear();
+//         for (ShopModel shopModel:shopModels){
+//             String shopName1 = shopModel.getShopName();
+//             productModels.clear();
+//             db.collection("Shop/"+shopName1+"/Products").addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                 @Override
+//                 public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                     if(error==null){
+//                         List<ProductModel> data = value.toObjects(ProductModel.class);
+//                         productModels.addAll(data);
+//                         for (ProductModel productModel:productModels){
+//                             AllDataModel temp = new AllDataModel(shopModel.getShopName(), shopModel.getShopLocation(), shopModel.getContactNumber(), productModel.getProductName(), productModel.getPrice(), productModel.getQuantity());
+//                             allDataModels.add(temp);
+//                         }
+//                         if (productModels.isEmpty()) {
+//                             Toast.makeText(getContext(), "No data Found", Toast.LENGTH_SHORT).show();
+//                         }
+//                         ProductModel productModel = productModels.get(0);
+//                         Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
+//                     }
+//                     else {
+//                         Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
+//                     }
+//                 }
+//             });
+//         }
+//         rcvData.setLayoutManager(new LinearLayoutManager(getContext()));
+//         rcvData.setAdapter(new AllDataAdaptor(getContext(), allDataModels));
+// //        if (allDataModels.isEmpty()){
+// //            Toast.makeText(getContext(), "No data found", Toast.LENGTH_SHORT).show();
+// //        }
+//         searchView = view.findViewById(R.id.searchView);
+//         searchView.clearFocus();
+//         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//             @Override
+//             public boolean onQueryTextSubmit(String query) {
+//                 return false;
+//             }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                filterList(newText);
-                return false;
-            }
-        });
+//             @Override
+//             public boolean onQueryTextChange(String newText) {
+//                 filterList(newText);
+//                 return false;
+//             }
+//         });
         return view;
     }
 
-    private void filterList(String newText) {
-        filteredList = new ArrayList<>();
-        for (AllDataModel allDataModel : allDataModels){
-            if (allDataModel.getProductName().toLowerCase().contains(newText.toLowerCase())){
-                filteredList.add(allDataModel);
-            }
+//     private void filterList(String newText) {
+//         filteredList = new ArrayList<>();
+//         for (AllDataModel allDataModel : allDataModels){
+//             if (allDataModel.getProductName().toLowerCase().contains(newText.toLowerCase())){
+//                 filteredList.add(allDataModel);
+//             }
 
-        }
-        if (filteredList.isEmpty()){
-            Toast.makeText(getContext(), "No data found", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            rcvData.setLayoutManager(new LinearLayoutManager(getContext()));
-            rcvData.setAdapter(new AllDataAdaptor(getContext(), filteredList));
-        }
-    }
+//         }
+//         if (filteredList.isEmpty()){
+//             Toast.makeText(getContext(), "No data found", Toast.LENGTH_SHORT).show();
+//         }
+//         else {
+//             rcvData.setLayoutManager(new LinearLayoutManager(getContext()));
+//             rcvData.setAdapter(new AllDataAdaptor(getContext(), filteredList));
+//         }
+//     }
 
-    private void getAllData() {
-        allDataModels = new ArrayList<>();
-        allDataModels.clear();
-        if (shopModels.isEmpty()){
-            Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
-        }
-        for (ShopModel shopModel:shopModels){
-            String shopName1 = shopModel.getShopName();
-            productModels.clear();
-            db.collection("Shop/"+shopName1+"/Products").addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                    if(error==null){
-                        List<ProductModel> data = value.toObjects(ProductModel.class);
-                        productModels.addAll(data);
-                        for (ProductModel productModel:productModels){
-                            AllDataModel temp = new AllDataModel(shopModel.getShopName(), shopModel.getShopLocation(), shopModel.getContactNumber(), productModel.getProductName(), productModel.getPrice(), productModel.getQuantity());
-                            allDataModels.add(temp);
-                        }
-                        if (productModels.isEmpty()) {
-                            Toast.makeText(getContext(), "No data Found", Toast.LENGTH_SHORT).show();
-                        }
-                        ProductModel productModel = productModels.get(0);
-                        Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-        rcvData.setLayoutManager(new LinearLayoutManager(getContext()));
-        rcvData.setAdapter(new AllDataAdaptor(getContext(), allDataModels));
-    }
+//     private void getAllData() {
+//         allDataModels = new ArrayList<>();
+//         allDataModels.clear();
+//         if (shopModels.isEmpty()){
+//             Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
+//         }
+//         for (ShopModel shopModel:shopModels){
+//             String shopName1 = shopModel.getShopName();
+//             productModels.clear();
+//             db.collection("Shop/"+shopName1+"/Products").addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                 @Override
+//                 public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                     if(error==null){
+//                         List<ProductModel> data = value.toObjects(ProductModel.class);
+//                         productModels.addAll(data);
+//                         for (ProductModel productModel:productModels){
+//                             AllDataModel temp = new AllDataModel(shopModel.getShopName(), shopModel.getShopLocation(), shopModel.getContactNumber(), productModel.getProductName(), productModel.getPrice(), productModel.getQuantity());
+//                             allDataModels.add(temp);
+//                         }
+//                         if (productModels.isEmpty()) {
+//                             Toast.makeText(getContext(), "No data Found", Toast.LENGTH_SHORT).show();
+//                         }
+//                         ProductModel productModel = productModels.get(0);
+//                         Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
+//                     }
+//                     else {
+//                         Toast.makeText(getContext(), "productModel.getProductName()", Toast.LENGTH_SHORT).show();
+//                     }
+//                 }
+//             });
+//         }
+//         rcvData.setLayoutManager(new LinearLayoutManager(getContext()));
+//         rcvData.setAdapter(new AllDataAdaptor(getContext(), allDataModels));
+//     }
 
-    public void getShopData(){
-        shopModels = new ArrayList<>();
-//        shopName = MainActivity.shopName;
-        db.collection("Shop").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if(error==null){
-                    List<ShopModel> data = value.toObjects(ShopModel.class);
-                    shopModels.addAll(data);
-                    ShopModel dataModel = shopModels.get(0);
-                    Toast.makeText(getContext(), dataModel.getShopName(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
+//     public void getShopData(){
+//         shopModels = new ArrayList<>();
+// //        shopName = MainActivity.shopName;
+//         db.collection("Shop").addSnapshotListener(new EventListener<QuerySnapshot>() {
+//             @Override
+//             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                 if(error==null){
+//                     List<ShopModel> data = value.toObjects(ShopModel.class);
+//                     shopModels.addAll(data);
+//                     ShopModel dataModel = shopModels.get(0);
+//                     Toast.makeText(getContext(), dataModel.getShopName(), Toast.LENGTH_SHORT).show();
+//                 }
+//             }
+//         });
+//     }
 }
